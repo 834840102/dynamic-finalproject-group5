83de5f864c100d402f241ea2c2f166482fcec7fc
834840102 <72417399+834840102@users.noreply.github.com>
2021-04-17 13:30:13 +0800

    Revert "John-commit"

--- group5_app/stories/GoalBtn.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import GoalBtn from '../comps/GoalBtn';
-
-export default {
-    title:"Example/GoalBtn",
-    component:<GoalBtn />
-}
-
-export const MyGoalBtn = () => <GoalBtn />
--- group5_app/stories/SugestionBox.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SuggestionBox from '../comps/SuggestionBox';
-
-export default {
-    title:"Example/SuggestionBox",
-    component:<SuggestionBox />
-}
-
-export const MySuggestionBox = () => <SuggestionBox />
