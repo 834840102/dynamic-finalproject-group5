d82250fcfa66d1c2dabe5f620767002a371a4111
Yuma Hosomi <swj150mtih@gmail.com>
2021-04-19 15:22:35 -0700

    Yuma commit

--- group5_app/comps/Logo/index.js
+++ group5_app/comps/Logo/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const LogotImg = styled.img`
+const LogoImg = styled.img`
     width:43px;
     height:61px;
 `; 
 
-const Logo = ({
-    width=43,
-    height=61,
-    src="vercel.svg"
-}) =>{
-    return <LogoImg width={width} height={height} src={src} />
+const Logo = ({}) =>{
+    return <LogoImg src="/vercel.svg" />
 }
 
 export default Logo;
