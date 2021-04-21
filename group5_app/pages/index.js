4812baa841846bccb7bbc20a1d5642f7c903311d
Nhung Tran <71236879+CharleneTran@users.noreply.github.com>
2021-04-20 22:08:58 -0700

    charlene commit

--- group5_app/pages/index.js
+++ group5_app/pages/index.js
@@ -1,6 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+import LogoComponent from '../comps/LogoComponent'
 
-export default function Home() {
-  return 
-}
+const PageLayoutContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width:400px;
+    height:750px;
+    background-color: #F2F0EB;
+    border: 55px solid #9FCBBB;
+    border-left: 15px solid #9FCBBB;
+    border-right: 15px solid #9FCBBB;
+    border-radius: 45px;
+`;
+
+
+export default function Home(){
+    return <PageLayoutContainer><LogoComponent width="120px" height="170px" address="/welcomepage"/></PageLayoutContainer>
+} 
