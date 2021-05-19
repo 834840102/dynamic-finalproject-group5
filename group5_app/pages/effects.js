ef4529a4e8fdf2cdc16db58705c7571f25386017
Nhung Tran <71236879+CharleneTran@users.noreply.github.com>
2021-05-19 15:01:01 -0700

    commit

--- group5_app/pages/effects.js
+++ group5_app/pages/effects.js
@@ -22,89 +22,76 @@ const PageLayoutContainer = styled.div`
       margin-right: 80%;
       cursor:pointer;
     }
-
     .text{
       display:flex;
       flex-direction: column;
       margin-top: -5%;
     }
-
     .text2{
       display:flex;
       flex-direction: column;
       margin-bottom: 4%;
     }
-
     .text4{
         display:flex;
         flex-direction: column;
         margin-bottom: 0%;
       }
   
-
     .MyImg{
       display:flex;
       flex-direction:column;
       cursor: pointer;
     }
-
     .MyImg1{
       display:flex;
       flex-direction:column;
       cursor: pointer;
       margin-bottom:20px;
     }
-
     .box {
         opacity:0;
-        margin-top: 20px;
+        margin-top: -10px;
     }
-    
     .MyImg1:hover + .box{
       opacity:1;
       animation: move 3s;
       position: relative;
     }
-
+    
     .MyImg2{
         display:flex;
         flex-direction:column;
         cursor: pointer;
         margin-bottom: 20px;
     }
-
     .MyImg2:hover + .box{
       opacity:1;
       animation: move 3s;
       position: relative;
     }
-
     .MyImg3{
         display:flex;
         flex-direction:column;
         cursor: pointer;
         margin-bottom: 20px;
     }
-
     .MyImg3:hover + .box{
       opacity:1;
       animation: move 3s;
       position: relative;
     }
-
     .MyImg4{
         display:flex;
         flex-direction:column;
         cursor: pointer;
         margin-bottom: 20px;
     }
-
     .MyImg4:hover + .box{
       opacity:1;
       animation: move 3s;
       position: relative;
     }
-
     .btn{
         display:flex;
         flex-grow: 5;
@@ -116,7 +103,6 @@ const PageLayoutContainer = styled.div`
         background-color: #6C9083;
         cursor: pointer;
       }
-
       @keyframes move {
         from {margin-right: -20%; }
         to {margin-right: 0; }
@@ -148,10 +134,10 @@ export default function Home() {
       <TextComponent Subtitle = "Decreased digestive function" Hint="" h3color="#33393E"/>
     </div>
     <div className="MyImg1">
-      <ImgComponents src="stomach.png" imgheight="80px" imgwidth="80px"/>
+      <ImgComponents src="stomach.png" imgheight="70px" imgwidth="70px"/>
     </div>
     <div className="box">
-      <SuggestionBox Boxheight="190px" Boxwidth="320px" title="" listyle="none" suggestion1="Fastfood contains a lot of calories and cholesterol, it will cause high blood pressure. If you want to burn off excess energy, you need to increase physical activity and exercise regularly, but fast food makes people more stagnant" />
+      <SuggestionBox Boxheight="150px" Boxwidth="300px" title="" listyle="none" suggestion1="Fastfood contains a lot of calories and cholesterol, it will cause high blood pressure. If you want to burn off excess energy, you need to increase physical activity and exercise regularly, but fast food makes people more stagnant" />
     </div>
 
 
@@ -159,30 +145,30 @@ export default function Home() {
       <TextComponent Subtitle = "Heart Diseases" Hint="" h3color="#33393E"/>
     </div>
     <div className="MyImg2">
-      <ImgComponents src="heart.png" imgheight="80px" imgwidth="80px"/>
+      <ImgComponents src="heart.png" imgheight="70px" imgwidth="70px"/>
     </div>
     <div className="box">
-      <SuggestionBox Boxheight="240px" Boxwidth="320px" title="" listyle="none" suggestion1="Junk foods have been identified as a major cause of heart diseases including myocardial infarction, cardiac arrest, and atherosclerosis. This is due to the fact that junk food contains excessive amount of low-density lipoproteins and cholesterol that get deposited on the inner linings of blood vessels." />
+      <SuggestionBox Boxheight="195px" Boxwidth="300px" title="" listyle="none" suggestion1="Junk foods have been identified as a major cause of heart diseases including myocardial infarction, cardiac arrest, and atherosclerosis. This is due to the fact that junk food contains excessive amount of low-density lipoproteins and cholesterol that get deposited on the inner linings of blood vessels." />
     </div>
  
     <div className="text4">
       <TextComponent Subtitle = "Respiratory Sytem" Hint="" h3color="#33393E"/>
     </div>
     <div className="MyImg3">
-      <ImgComponents src="brain.png" imgheight="80px" imgwidth="80px"/>
+      <ImgComponents src="brain.png" imgheight="70px" imgwidth="70px"/>
     </div>
     <div className="box">
-      <SuggestionBox Boxheight="240px" Boxwidth="320px" title="" listyle="none" suggestion1="Junk foods are high in saturated fats and Omega-6 fatty acids, individuals are likely to get depressed. When a person consumes junk food in excess, body finds difficult to digest and needs to spend high amount of blood and enzymes as a result, the person feels drowsy and suffers with reduced concentration." />
+      <SuggestionBox Boxheight="220px" Boxwidth="300px" title="" listyle="none" suggestion1="Junk foods are high in saturated fats and Omega-6 fatty acids, individuals are likely to get depressed. When a person consumes junk food in excess, body finds difficult to digest and needs to spend high amount of blood and enzymes as a result, the person feels drowsy and suffers with reduced concentration." />
     </div>
 
     <div className="text4">
       <TextComponent Subtitle = "Poor Nutrition" Hint="" h3color="#33393E"/>
     </div>
     <div className="MyImg4">
-      <ImgComponents src="balance.png" imgheight="80px" imgwidth="80px"/>
+      <ImgComponents src="balance.png" imgheight="70px" imgwidth="70px"/>
     </div>
     <div className="box">
-      <SuggestionBox Boxheight="190px" Boxwidth="320px" title="" listyle="none" suggestion1="When you eat too much junk food, you neglect to eat nutritious food like fruits and vegetables and other wholesome food. This means that your body does not get the necessary nutrition it needs and you end up with a weak immune system." />
+      <SuggestionBox Boxheight="170px" Boxwidth="300px" title="" listyle="none" suggestion1="When you eat too much junk food, you neglect to eat nutritious food like fruits and vegetables and other wholesome food. This means that your body does not get the necessary nutrition it needs and you end up with a weak immune system." />
     </div>
 
     <div className="btn">
