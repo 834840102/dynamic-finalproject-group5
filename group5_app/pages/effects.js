6507ffb061b84e95ea43afae770098bb134c1593
Nhung Tran <71236879+CharleneTran@users.noreply.github.com>
2021-05-19 11:36:00 -0700

    commit

--- group5_app/pages/effects.js
+++ group5_app/pages/effects.js
@@ -148,7 +148,7 @@ export default function Home() {
       <TextComponent Subtitle = "Decreased digestive function" Hint="" h3fontsize="18px" h3color="#33393E"/>
     </div>
     <div className="MyImg1">
-      <ImgComponents src="stomach.png" imgheight="100px" imgwidth="100px"/>
+      <ImgComponents src="stomach.png" imgheight="80px" imgwidth="80px"/>
     </div>
     <div className="box">
       <SuggestionBox Boxheight="190px" Boxwidth="320px" title="" listyle="none" suggestion1="Fastfood contains a lot of calories and cholesterol, it will cause high blood pressure. If you want to burn off excess energy, you need to increase physical activity and exercise regularly, but fast food makes people more stagnant" />
@@ -159,7 +159,7 @@ export default function Home() {
       <TextComponent Subtitle = "Heart Diseases" Hint="" h3fontsize="18px" h3color="#33393E"/>
     </div>
     <div className="MyImg2">
-      <ImgComponents src="heart.png" imgheight="100px" imgwidth="100px"/>
+      <ImgComponents src="heart.png" imgheight="80px" imgwidth="80px"/>
     </div>
     <div className="box">
       <SuggestionBox Boxheight="240px" Boxwidth="320px" title="" listyle="none" suggestion1="Junk foods have been identified as a major cause of heart diseases including myocardial infarction, cardiac arrest, and atherosclerosis. This is due to the fact that junk food contains excessive amount of low-density lipoproteins and cholesterol that get deposited on the inner linings of blood vessels." />
@@ -169,7 +169,7 @@ export default function Home() {
       <TextComponent Subtitle = "Respiratory Sytem" Hint="" h3fontsize="18px" h3color="#33393E"/>
     </div>
     <div className="MyImg3">
-      <ImgComponents src="brain.png" imgheight="100px" imgwidth="100px"/>
+      <ImgComponents src="brain.png" imgheight="80px" imgwidth="80px"/>
     </div>
     <div className="box">
       <SuggestionBox Boxheight="240px" Boxwidth="320px" title="" listyle="none" suggestion1="Junk foods are high in saturated fats and Omega-6 fatty acids, individuals are likely to get depressed. When a person consumes junk food in excess, body finds difficult to digest and needs to spend high amount of blood and enzymes as a result, the person feels drowsy and suffers with reduced concentration." />
@@ -179,7 +179,7 @@ export default function Home() {
       <TextComponent Subtitle = "Poor Nutrition" Hint="" h3fontsize="18px" h3color="#33393E"/>
     </div>
     <div className="MyImg4">
-      <ImgComponents src="balance.png" imgheight="100px" imgwidth="100px"/>
+      <ImgComponents src="balance.png" imgheight="80px" imgwidth="80px"/>
     </div>
     <div className="box">
       <SuggestionBox Boxheight="190px" Boxwidth="320px" title="" listyle="none" suggestion1="When you eat too much junk food, you neglect to eat nutritious food like fruits and vegetables and other wholesome food. This means that your body does not get the necessary nutrition it needs and you end up with a weak immune system." />
